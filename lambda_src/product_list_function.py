871f72489d0d2405a4d8d97ecfd9764f704fc3a3
Alvaro Lupercio <aluper@amazon.com>
2025-03-09 18:38:25 +0000

    Add ApiGateway to trigger Lambda and read DynamoDB table

--- lambda_src/product_list_function.py
+++ lambda_src/product_list_function.py
@@ -1,5 +1,6 @@
 import boto3
 import os
+import json
 
 def handler(event, context):
     # Initialize DynamoDB client
@@ -7,15 +8,16 @@ def handler(event, context):
 
     # Get table name from environment variable
     table_name = os.environ.get('TABLE_NAME')
+
     table = dynamodb.Table(table_name)
 
     # Scan the table
     response = table.scan()
+
     items = response.get('Items', [])
 
     # Return the items
     return {
         'statusCode': 200,
-        'body': items
+        'body': json.dumps(items, default=str)
     }
-
--- serverless_app/serverless_app_stack.py
+++ serverless_app/serverless_app_stack.py
@@ -1,10 +1,12 @@
 from aws_cdk import (
+    CfnOutput,
     Duration,
     RemovalPolicy,
     Stack,
     Tags,
     aws_dynamodb as dynamodb,
     aws_lambda as _lambda,
+    aws_apigateway as apigateway
 )
 from constructs import Construct
 
@@ -16,29 +18,47 @@ class ServerlessAppStack(Stack):
         # DynamoDB Table definition
         #
         products_table = dynamodb.Table(
-                self, 'ProductsTable',
-                partition_key=dynamodb.Attribute(
-                    name='id',
-                    type=dynamodb.AttributeType.STRING
-                    ),
-                removal_policy=RemovalPolicy.DESTROY)
+            self, 'ProductsTable',
+            partition_key=dynamodb.Attribute(
+                name='id',
+                type=dynamodb.AttributeType.STRING
+                ),
+            removal_policy=RemovalPolicy.DESTROY)
 
 
         # Lambda Function definition and tags
         product_list_function = _lambda.Function(
-                self, 'ProductListTable',
-                runtime=_lambda.Runtime.PYTHON_3_12,
-                handler='product_list_function.handler',
-                code=_lambda.Code.from_asset('lambda_src'),
-                environment={
-                    'TABLE_NAME': products_table.table_name
-                },
-                timeout=Duration.minutes(15))
+            self, 'ProductListTable',
+            runtime=_lambda.Runtime.PYTHON_3_12,
+            handler='product_list_function.handler',
+            code=_lambda.Code.from_asset('lambda_src'),
+            environment={
+                'TABLE_NAME': products_table.table_name
+            },
+            timeout=Duration.minutes(15))
 
 
         # Grant read permissions on DynamoDB table to Lambda function 
         products_table.grant_read_data(product_list_function)
 
+        # ApiGateway definition
+        api = apigateway.LambdaRestApi(
+            self, 'ItemsApi',
+            handler=product_list_function,
+            proxy=False
+        )
+
+        # Add resource and method to apigateway
+        items = api.root.add_resource("items")
+        items.add_method("GET")
+
+        # Output the API URL
+        CfnOutput(
+            self, 'ApiUrl',
+            value=f"{api.url}items"
+        )
+
 
         Tags.of(products_table).add('env', 'dev')
         Tags.of(product_list_function).add('env', 'dev')
+        Tags.of(api).add('env', 'dev')

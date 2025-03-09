4af8b1041c27e9d59a37b41a96dfdc492862d33c
Alvaro Lupercio <aluper@amazon.com>
2025-03-09 00:56:09 +0000

    Add DynamoDB table for test data

--- serverless_app/serverless_app_stack.py
+++ serverless_app/serverless_app_stack.py
@@ -1,7 +1,8 @@
 from aws_cdk import (
-    # Duration,
+    RemovalPolicy,
     Stack,
-    # aws_sqs as sqs,
+    Tags,
+    aws_dynamodb as dynamodb,
 )
 from constructs import Construct
 
@@ -10,10 +11,12 @@ class ServerlessAppStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        products_table = dynamodb.Table(self, 'ProductsTable',
+                                        partition_key=dynamodb.Attribute(
+                                            name='id',
+                                            type=dynamodb.AttributeType.STRING
+                                        ),
+                                        removal_policy=RemovalPolicy.DESTROY)
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "ServerlessAppQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+
+        Tags.of(products_table).add('env', 'dev')
